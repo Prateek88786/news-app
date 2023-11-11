3129d109063d2ebbba95fe6233131e3feade5a72
Prateek88786 <141042244+Prateek88786@users.noreply.github.com>
2023-11-11 22:23:26 +0530

    Icons added and title changed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,7 +15,7 @@ function navbar(props){
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
-            <Link onClick={props.unset} className="navbar-brand" to="/">News</Link>
+            <Link onClick={props.unset} className="navbar-brand" to="/">newsMarket.com</Link>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item mx-2">
